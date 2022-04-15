639a27afa3377bed4d65f2dcc0bad6dfa2f7db08
Rafael Barros <rafaelnogueirabarr@gmail.com>
2022-04-15 18:56:22 -0300

    refactor: best practices

--- js/main.js
+++ js/main.js
@@ -1,158 +1,101 @@
+const screen = document.querySelector(".tela");
+const currentOperationELM = document.querySelector(".current-operation");
+const calculator = document.querySelector(".calculator");
 
-const tela = document.querySelector(".tela");
-const result = document.querySelector('[data-type="result"]');
-const clearRow = document.querySelector('[data-type="cleanRow"]');
-const clearAll = document.querySelector('[data-type="cleanAll"]')
-const clearLast = document.querySelector('[data-type="cleanLast"]');
-const mem  = document.querySelector('.opShow');
-let op = "";
-let resultOp = "";
-const operations = ["+","÷","x","-"];
-let y = 0;
+let previousOperation = null;
+let mathCurrent = 0;
+let buffer = "";
 
-function insert(number) {
+calculator.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const value = e.target.innerText;
+    if (isNaN(Number(value)) && value !== ",") {
+      handleSymbol(value);
+    } else {
+      if (buffer === "0") buffer = value;
+      else {
+        if (buffer.includes(".")) {
+          buffer += value.replace(",", "");
+        } else {
+          buffer += value.replace(",", ".");
+        }
+      }
+    }
 
-let numbers = tela.value;
-tela.value += number;
+    render();
+  }
+});
 
-clearLast.onclick = () =>{
-    if(tela.value !== "" && tela.value !== 0 && op === "") {
-        tela.value = tela.value.substr(0, tela.value.length -1)
-} else {
-        clearLast.disabled = true;
+function handleSymbol(value) {
+  const symbols = {
+    "=": () => {
+      if (previousOperation === null) return;
+      flushOperation(Number(buffer));
+      buffer = String(mathCurrent);
+      previousOperation = null;
+    },
+    C: () => {
+      deepClean();
+    },
+    CE: () => {
+      buffer = "0";
+    },
+    "[x]": () => {
+      if (buffer.length === 1) buffer = "0";
+      else buffer = buffer.substring(0, buffer.length - 1);
+    },
+  };
+
+  const mathSymbol = symbols[value];
+  if (mathSymbol) mathSymbol();
+  else handleMath(value);
 }
-    if(tela.value !== "" && mem.textContent !== "") {
-        tela.value = tela.value.substr(0, tela.value.length -1)
-} else {
-         clearLast.disabled = true;
+
+function handleMath(value) {
+  const intBuffer = Number(buffer);
+  if (mathCurrent === 0) {
+    mathCurrent = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperation = value;
+  buffer = "0";
 }
+
+function flushOperation(buffer) {
+  const mathSignals = {
+    "+": () => {
+      mathCurrent += buffer;
+    },
+    "-": () => {
+      mathCurrent -= buffer;
+    },
+    x: () => {
+      mathCurrent *= buffer;
+    },
+    "÷": () => {
+      mathCurrent /= buffer;
+    },
+  };
+
+  const doMath = mathSignals[previousOperation];
+  if (doMath) doMath();
 }
-    clearRow.onclick = () => {
-        if(tela.value !== "" || tela.value !== 0 && mem.textContent !== 0 || mem.textContent !== "") {
-                    tela.value = "";
-                } else {
-                    clearRow.disabled = true;
-                } 
-                if(tela.value !== "" || tela.value !== 0 || mem.textContent === "" || mem.textContent === 0) {
-                    tela.value = "";
-                } else {
-                    clearRow.disabled = true;
-                }
-            }
-            clearAll.onclick = () =>{
-                op = "";
-                tela.value = "";
-                mem.textContent = "";
-                document.querySelector('[data-type="buttonSUM"]').disabled = false;
-                document.querySelector('[data-type="buttonMinus"]').disabled = false;
-                document.querySelector('[data-type="buttonX"]').disabled = false;
-                document.querySelector('[data-type="buttonDivide"]').disabled = false;
-            }
 
-    if(tela.value.charAt(0) !== "+" &&
-        tela.value.charAt(0) !== "-"&&
-        tela.value.charAt(0) !== "x" &&
-        tela.value.charAt(0) !== "÷") {
-            
-        if(operations.includes(tela.value.charAt(numbers.length))) {
-            document.querySelector('[data-type="buttonSUM"]').disabled = true;
-            document.querySelector('[data-type="buttonMinus"]').disabled = true;
-            document.querySelector('[data-type="buttonX"]').disabled = true;
-            document.querySelector('[data-type="buttonDivide"]').disabled = true;
-            mem.textContent = tela.value;
+function deepClean() {
+  buffer = "0";
+  mathCurrent = 0;
+  previousOperation = null;
+  currentOperationELM.innerHTML = "";
+}
 
-                operation(tela.value.charAt(numbers.length))
+function render() {
+  const signals = ["+", "-", "x", "÷"];
 
-                if(tela.value.substr(0, mem.textContent.length) === mem.textContent){
-                    tela.value = tela.value.substr(mem.textContent, tela.value);
-                }
-            }
+  if (!isNaN(mathCurrent) && signals.includes(previousOperation)) {
+    currentOperationELM.innerHTML = `${mathCurrent} ${previousOperation} ${buffer} =`;
+  }
 
-        } else { 
-            tela.value = "";
-        }
-
-        if(op !== "" && tela.value !== "" && mem.textContent !== ""){
-            let numb = mem.textContent + tela.value;
-            result.onclick = () => operationDefined(numb.split(op));
-        }
-    }
-
-    function operation(opr) {
-        if(opr === '+') { return op = '+'}
-        if(opr === '-') { return op = '-'}
-        if(opr === '÷') { return op = '÷'}
-        if(opr === 'x') { return op = 'x'}
-    }
-
-    const operationDefined = (numbers) =>{
-        n1 = parseFloat(numbers[0]);
-        n2 = parseFloat(numbers[1]);
-
-       if(op === "+") {
-            resultOp = n1 + n2;
-       }
-       if(op === "-") {
-            resultOp = n1 - n2;
-       }
-       if( op === "÷" ) {
-           resultOp = n1 / n2;
-       }
-       if( op === "x") {
-           resultOp = n1 * n2;
-       }
-       if(resultOp !== "") {
-        clearLast.onclick = () =>{
-            mem.textContent = "";
-        } 
-        clearRow.onclick = () =>{
-            if(tela.value !== "" || tela.value !== 0 || mem.textContent !== "" || mem.textContent !== 0) {
-                tela.value = "";
-                mem.textContent = "";
-            } 
-        }
-
-       }
-    
-       mem.textContent = ` ${mem.textContent.substr(0, mem.textContent.length -1)} ${op} ${n2} =`
-       
-       if(mem.textContent.length >= 3) {
-
-           result.onclick =  evalopMem;
-        }
-       
-       document.querySelector('[data-type="buttonSUM"]').disabled = false;
-       document.querySelector('[data-type="buttonMinus"]').disabled = false;
-       document.querySelector('[data-type="buttonX"]').disabled = false;
-       document.querySelector('[data-type="buttonDivide"]').disabled = false;
-
-       return tela.value = resultOp;
-    }
-
-
-    function evalopMem() {
-        if(tela.value !== "" && mem.value !== "" && resultOp !== "") {
-        let th = mem.textContent
-        let thr = th.replace("=", "");
-        let thrs = thr.split(op)
-        n1n = parseFloat(thrs[0]);
-        n2n = parseFloat(thrs[1]);
-
-        mem.textContent = `${parseInt(tela.value)} ${op} ${parseInt(n2n)} =`
-   
-        if(op === "+") {
-            resultOp = n1n + n2n
-       }
-       if(op === "-") {
-            resultOp = n1n - n2n;
-       }
-       if( op === "÷" ) {
-           resultOp = n1n / n2n;
-       }
-       if( op === "x") {
-           resultOp = n1n * n2n;
-       }
-    
-       tela.value = resultOp;
-    }
+  screen.value = Number(buffer) || buffer.includes(".") ? buffer : 0;
 }
